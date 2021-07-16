34715312affa1ea12bc0ed00284a1ad90f2ddcb9
Ivan Alexis <jimenezivanalexis@gmail.com>
2021-07-15 23:39:37 -0600

    Adds BreadCrum to the movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Movie = () => <div>Movie</div>;
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+import BreadCrum from './BreadCrum';
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if(loading) return <Spinner/>;
+  if(error) return <div>Something went wrong :(</div>;
+
+  return (
+    <>
+      <BreadCrum movieTitle={ movie.original_title } />
+    </>
+  );
+};
 
 export default Movie;
